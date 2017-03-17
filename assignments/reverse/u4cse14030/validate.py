f1a0d71d3191de5ea983908bff323268b20ec3bb
Haritha28 <haritha.h20@gmail.com>
2017-03-17 22:07:43 +0530

    extracting bits not working

--- assignments/reverse/u4cse14030/validate.py
+++ assignments/reverse/u4cse14030/validate.py
@@ -4,13 +4,19 @@ import hashlib
 import struct
 import numpy
 
+#array = ['0x5daac65f720be8c9',' 0x8c92b50d1c2d1e4f','0xa6fd433408510ea4','0x0e9296eddd450b03','0x84db12124b70fbd9','0x091d7b508606ffac','0xfd41e1b9fd423b8f','0x56bd6aca483c8c74','0x037c7f5864b34642','0xb488202e8ea9111b','0x88af43a6fe478cda','0xaa4fc294b8b380b6','0x5b1c0d7ed87b9535','0x4a9b68be4888bc63',' 0x0b1a4657555b0964','0xf0e4afaab7a436c1','0x3b031b0100000408']
+
 input = raw_input()
 list =  input.split( " ")
 count_check = len(input)
 
-def xorfun(value):
-    return hex(( (number >> 64 ) ^ number) & 0xffffffffffffffff)
-
+# def loop1(xorval):
+#     temp = xorval
+#     counter = 0
+#     while ( counter != 64 ):
+#         shiftright_value = hex(temp >> counter)
+#         print shiftright_value
+#         counter+=16
 def pair_reverse(hashval):
     n = len(hashval)/2
     fmt = '%dh' % n
@@ -22,8 +28,12 @@ def check_key(list):
     flag = 1
     hashval =  hashlib.md5(user_name).hexdigest()
     value = pair_reverse(hashval)
-    xorval = xorfun(value)
-    print xorval
+    lower_hex = hex(value >> 64)
+    print lower_hex
+    higher_hex = hex(value & 0xffffffffffffffff)
+    print higher_hex
+    #result_128 = ( (value >> 64 ) ^ value) & 0xffffffffffffffff
+    #print xorval
 
 if count_check < 1:
     print "USAGE: USERNAME KEY"
